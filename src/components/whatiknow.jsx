099a82bcc5f53f4223a3e964e69d1c3bf947729d
Priyanshu Chahar <148974998+E5F6@users.noreply.github.com>
2024-05-08 14:53:09 +0000

    portfolio

--- src/components/whatiknow.jsx
+++ src/components/whatiknow.jsx
@@ -5,14 +5,15 @@ import {
   RiReactjsFill,
   RiNextjsFill,
 } from "react-icons/ri";
-import { SiMysql, SiMongodb } from "react-icons/si";
+import { SiMysql, SiMongodb , SiExpress } from "react-icons/si";
 import { FaGitAlt, FaGithub, FaLanguage } from "react-icons/fa";
 import { FaWindows, FaLinux, FaUbuntu } from "react-icons/fa6";
 import { FaPython, FaJava } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa";
 
 const WhatIKnow = () => {
-  const frontends = [
+
+  const skills = [
     {
       name: "HTML",
       icon: <SiHtml5 />,
@@ -29,9 +30,6 @@ const WhatIKnow = () => {
       name: "Typescript",
       icon: <SiTypescript />,
     },
-  ];
-
-  const frameworks = [
     {
       name: "Tailwind",
       icon: <RiTailwindCssFill />,
@@ -48,16 +46,14 @@ const WhatIKnow = () => {
       name: "Next JS",
       icon: <RiNextjsFill />,
     },
-  ];
-
-  const backends = [
     {
       name: "NodeJS",
       icon: <FaNodeJs />,
     },
-  ];
-
-  const databases = [
+    {
+      name: "ExpressJS",
+      icon: <SiExpress />,
+    },
     {
       name: "MySql",
       icon: <SiMysql />,
@@ -66,9 +62,6 @@ const WhatIKnow = () => {
       name: "MongoDB",
       icon: <SiMongodb />,
     },
-  ];
-
-  const versionControls = [
     {
       name: "Git",
       icon: <FaGitAlt />,
@@ -77,9 +70,6 @@ const WhatIKnow = () => {
       name: "Github",
       icon: <FaGithub />,
     },
-  ];
-
-  const operatingSystems = [
     {
       name: "Windows",
       icon: <FaWindows />,
@@ -92,9 +82,6 @@ const WhatIKnow = () => {
     //   name: "Ubuntu",
     //   icon: <FaUbuntu />,
     // },
-  ];
-
-  const languages = [
     {
       name: "Python",
       icon: <FaPython />,
@@ -119,120 +106,18 @@ const WhatIKnow = () => {
         <h1 className="text-2xl md:text-3xl text-slate-200 font-medium self-start">
           What I Know
         </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-start items-stretch gap-5">
-          {/* Frontend */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Frontend
-            </h1>
+          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 border-b-2 border-cyan-600 card">
             <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {frontends.map((frontend, index) => (
+              {skills.map((skill, index) => (
                 <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
+                  className="px-4 py-2 flex gap-1 text-slate-300 hover:text-cyan-600 font-medium rounded-md items-center transition ease-in-out cursor-pointer skill"
                   key={index}
                 >
-                  {frontend.icon} {frontend.name}
+                  {skill.icon} {skill.name}
                 </p>
               ))}
             </div>
           </div>
-          {/* Framework */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Frameworks
-            </h1>
-            <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {frameworks.map((framework, index) => (
-                <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
-                  key={index}
-                >
-                  {framework.icon} {framework.name}
-                </p>
-              ))}
-            </div>
-          </div>
-          {/* Backend */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Backend
-            </h1>
-            <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {backends.map((backend, index) => (
-                <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
-                  key={index}
-                >
-                  {backend.icon} {backend.name}
-                </p>
-              ))}
-            </div>
-          </div>
-          {/* Database */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Database
-            </h1>
-            <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {databases.map((database, index) => (
-                <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
-                  key={index}
-                >
-                  {database.icon} {database.name}
-                </p>
-              ))}
-            </div>
-          </div>
-          {/* Version Control */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Version Control
-            </h1>
-            <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {versionControls.map((versionControl, index) => (
-                <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
-                  key={index}
-                >
-                  {versionControl.icon} {versionControl.name}
-                </p>
-              ))}
-            </div>
-          </div>
-          {/* Operating System */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Operating System
-            </h1>
-            <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {operatingSystems.map((operatingSystem, index) => (
-                <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
-                  key={index}
-                >
-                  {operatingSystem.icon} {operatingSystem.name}
-                </p>
-              ))}
-            </div>
-          </div>
-          {/* Languages */}
-          <div className="flex flex-col justify-start items-center gap-3 rounded-md p-5 max-w-[400px] border-b-2 border-cyan-600 card">
-            <h1 className="text-xl font-medium self-start text-slate-200">
-              Languages
-            </h1>
-            <div className="flex flex-wrap justify-start items-center gap-2 self-start">
-              {languages.map((language, index) => (
-                <p
-                  className="px-3 py-2 text-sm flex gap-1 text-slate-300 rounded-md items-center transition ease-in-out cursor-pointer skill"
-                  key={index}
-                >
-                  {language.icon} {language.name}
-                </p>
-              ))}
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   );
