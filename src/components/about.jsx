e02de65977b1a691abcc8fd576fce1578e507de5
Priyanshu Chahar <148974998+E5F6@users.noreply.github.com>
2024-05-09 18:45:18 +0530

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,19 +10,19 @@ const About = () => {
         </h1>
         <div className="flex justify-start items-center gap-3 px-5 py-5 rounded-md w-full card">
           <div className="col-span-1 flex flex-col justify-start items-center gap-3">
-            <p className="text-slate-300 font-medium">
+            <p className="text-slate-300 font-medium self-start">
               My name is Priyanshu Chahar and I&apos;m a computer science student
               with a passion for programming. I&apos;m deeply interested in web
               development and enjoy exploring new technologies to build
               innovative solutions.
             </p>
-            <p className="text-slate-300 font-medium">
+            <p className="text-slate-300 font-medium self-start">
               As a full stack developer, I&apos;m proficient in HTML, CSS,
               JavaScript, TypeScript, Tailwind CSS, Bootstrap, React.js,
               Next.js, and Node.js. I&apos;m always eager to expand my skill set and
               learn new technologies to stay updated with industry trends.
             </p>
-            <p className="text-slate-300 font-medium">
+            <p className="text-slate-300 font-medium self-start">
               Outside of programming, I enjoy spending my free time on hobbies
               like coding personal projects. I find joy in solving complex
               problems and turning ideas into reality through code.
